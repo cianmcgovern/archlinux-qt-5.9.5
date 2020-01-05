db5b3b2b60490527888e4b875693b40e723a18a0
Cian Mc Govern <cian@cianmcgovern.com>
2020-01-05 15:37:50 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -5,13 +5,15 @@ set -x
 
 sudo pacman -Syu --noconfirm
 
-export PACKAGER="Cian McGovern (Travis CI) <cian@cianmcgovern.com>"
+export PACKAGER="Cian McGovern <cian@cianmcgovern.com>"
 
 PACKAGES="qt5-base-595 qt5-xmlpatterns-595 qt5-declarative-595 qt5-location-595 qt5-quickcontrols-595 qt5-tools-595 qt5-webchannel-595 qt5-webengine-595 qt5-x11extras-595"
 
 for package in $PACKAGES; do
     cd $package
     makepkg -cCf -si --noconfirm
-    sha256sum $package-*.tar.xz
-    cd ..
+    for package_file in $package-*.tar.xz; do
+        sha256sum $package_file
+        curl --upload-file ./$package_file https://transfer.sh/${package_file}
+    cd ..   
 done
